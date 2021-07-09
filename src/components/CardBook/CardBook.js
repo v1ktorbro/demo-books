b598ed2f17b6cf3eb9f520814ebdf6fb2e37955f
Viktor Abrosimov <vitya.abrosimoff2016@yandex.ru>
2021-07-09 16:05:14 +0500

    feat(CardBook): получение данных через пропсы

--- src/components/CardBook/CardBook.js
+++ src/components/CardBook/CardBook.js
@@ -1,12 +1,12 @@
 import './CardBook.css';
 
-function CardBook() {
+function CardBook({ linkImage, title, description }) {
   return (
     <section className="card-book">
-        <img src="https://img4.labirint.ru/rc/29c8d0b98ac4798cee0f91279103c181/220x340/books71/705242/cover.jpg?1564210842" className="card-book__image" alt="Обложка книги" />
+        <img src={linkImage} className="card-book__image" alt="Обложка книги" />
       <div className="card-book__text-block" >
-        <h3 className="card-book__title">Мастер и Маргарита</h3>
-        <p className="card-book__description">«Ма́стер и Маргари́та» — роман Михаила Афанасьевича Булгакова, работа над которым началась в декабре 1928 года и продолжалась вплоть до смерти писателя. Роман относится к незавершённым произведениям; редактирование и сведение воедино черновых записей осуществляла после смерти мужа вдова писателя — Елена Сергеевна</p>
+        <h3 className="card-book__title">{title}</h3>
+        <p className="card-book__description">{description}</p>
       </div>
     </section>
   );
