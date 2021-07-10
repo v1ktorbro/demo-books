ef6df9f069c2e30c37078cfdb3eab8647c746c58
Viktor Abrosimov <vitya.abrosimoff2016@yandex.ru>
2021-07-10 11:09:28 +0500

    refactor(BtnsEditChange): добавить пропс для стиля компонента

--- src/components/BtnsEditChange/BtnsEditChange.js
+++ src/components/BtnsEditChange/BtnsEditChange.js
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './BtnsEditChange.css';
 
-function BtnsEditChange({ styleArea, placeholder, onCancel, defaultValueTextArea, onSubmit }) {
+function BtnsEditChange({ styleArea, styleComponent, placeholder, onCancel, defaultValueTextArea, onSubmit }) {
   const [valueTextArea, setValueTextArea] = useState(defaultValueTextArea);
   return (
-    <form className="btns-edit-change" onSubmit={(evt) => onSubmit(evt, valueTextArea)}>
+    <form className="btns-edit-change" style={styleComponent} onSubmit={(evt) => onSubmit(evt, valueTextArea)}>
       <textarea defaultValue={valueTextArea} className="btns-edit-change__textarea" placeholder={placeholder} style={styleArea} onChange={(evt) => setValueTextArea(evt.target.value)} />
       <div className="btns-edit-change__btns-container">
         <button className="btns-edit-change__save" type="submit">Сохранить</button>
