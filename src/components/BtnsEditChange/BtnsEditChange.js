a5dd16ba3534a73895832c6484b0bbf727c4bf92
Viktor Abrosimov <vitya.abrosimoff2016@yandex.ru>
2021-07-11 14:18:47 +0500

    feat(BtnsEditChange): вертска для под загрузку файлов

--- src/components/BtnsEditChange/BtnsEditChange.js
+++ src/components/BtnsEditChange/BtnsEditChange.js
@@ -1,18 +1,33 @@
 import { useState } from 'react';
 import './BtnsEditChange.css';
+import fileIcon from "../../images/file-icon.svg";
 
-function BtnsEditChange({ styleArea, styleComponent, placeholder, onCancel, defaultValueTextArea, onSubmit, labelHide }) {
+function BtnsEditChange({ styleArea, styleComponent, placeholder, onCancel, defaultValueTextArea, onSubmit, labelHide, ifFileLoadForm }) {
   const [valueTextArea, setValueTextArea] = useState(defaultValueTextArea);
   return (
     <form className="btns-edit-change" style={styleComponent} onSubmit={(evt) => onSubmit(evt, valueTextArea)}>
+    { ifFileLoadForm ?
+        <div className="btns-edit-change__input-file-wrapper">
+          <input name="imageFile" id="input__image-file" className="btns-edit-change__input-file" type="file" />
+          <label htmlFor="input__image-file" className="btns-edit-change__input-file-button">
+            <span className="btns-edit-change__input-file-icon-wrapper">
+              <img className="btns-edit-change__input-file-icon" src={fileIcon} alt="Выбрать файл" width="25" />
+            </span>
+            <span className="btns-edit-change__input-file-button-text">Выберите файл</span>
+          </label>
+        </div>
+      :
       <textarea defaultValue={valueTextArea} className="btns-edit-change__textarea" placeholder={placeholder} style={styleArea} onChange={(evt) => setValueTextArea(evt.target.value)} />
-      <div className="btns-edit-change__btns-container">
-        <button className="btns-edit-change__save" type="submit" onClick={labelHide}>Сохранить</button>
-        <button className="btns-edit-change__cancel" type="button" onClick={() => {
-          onCancel();
-          labelHide();
-        }} >Отменить</button>
-      </div>
+    }
+      { !ifFileLoadForm &&
+        <div className="btns-edit-change__btns-container">
+          <button className="btns-edit-change__save" type="submit" onClick={labelHide}>Сохранить</button>
+          <button className="btns-edit-change__cancel" type="button" onClick={() => {
+            onCancel();
+            labelHide();
+          }} >Отменить</button>
+        </div>
+      }
     </form>
   );
 }
